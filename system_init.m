3fee076d5bfd2e3e720d0b460e72c5582fe867f3
whydak13 <michal.gora997@gmail.com>
2016-05-27 14:45:08 +0200

    time vectors

--- system_init.m
+++ system_init.m
@@ -3,15 +3,32 @@ close all;
 
 ro = 2;
 u0 = 20;
-
+MSHDNS=1000; % mesh density, liczba kroków miêdzy prze³¹czeniami
 x0 = [0, 0.5, 0, 0.5, 0.25*pi,-0.5,0.25*pi,-0.5,1,0]';
 t = 0:0.001:0.25;
-u = u_bang_bang(t, [], [], u0);
+u1=[];
+u2=[];
+[ time ] = time_vectors( t, u1, u2 ,MSHDNS)
+[n,m]=size(time);
+for i=1:n
+    
+    
+    
+    
+end
+
+
+u = u_bang_bang(t, u1, u2, u0);
 [t, x] = rk4(@crane_rhs, u, t, x0);
+
 [~, psi] = rk4_b(@rhs_psi, u, t, -ro*x(:,length(x)));
 g = rhs_psi_u(t, x, psi);
 u_max=20;
 [ g_u ] = fun_g_u( g,u,u_max);
+
+
+
+
 
 subplot(3,1,1);
 plot(t, x([1 3 5 7],:));
---
+++ time_vectors.m
@@ -0,0 +1,29 @@
+function [ time ] = time_vectors( t, u1, u2 ,MSHDNS)
+
+
+    bangs=sort([u1 u2]);
+    tmp_t0=t(1);
+
+    T=t(end);
+    n=length(bangs);
+    time=zeros(n+1,MSHDNS)
+    if n>0
+        tmp_T=bangs(1);
+        time(1,:)=linspace(tmp_t0,tmp_T,MSHDNS);
+    end
+    if n>2
+        for i=2:n
+         tmp_t0=bangs(i-1); 
+         tmp_T=bangs(i); 
+         time(i,:)=linspace(tmp_t0,tmp_T,MSHDNS);
+
+        end
+    end
+    if n>0
+        tmp_t0=bangs(n);
+    else
+        tmp_t0=bangs(n);
+    end
+    time(n+1,:)=linspace(tmp_t0,T,MSHDNS);
+end
+
