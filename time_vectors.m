b0991df76e26152301379c4bd6be57bf6c4b91c9
whydak13 <michal.gora997@gmail.com>
2016-05-28 12:00:10 +0200

    poprawione

--- time_vectors.m
+++ time_vectors.m
@@ -14,18 +14,20 @@ function [ time, u_v1,u_v2 ] = time_vectors( t, u1, u2, u0 ,MSHDNS)
     u_v1=ones(n+1,MSHDNS)*u0;
     u_v2=ones(n+1,MSHDNS)*u0;
     
-   
+   %% Przelaczenie w zerze
      if any(u1==0)
-                    u_v1(1,:)=-u_v1(1,:);
-            end
+            u_v1(1,:)=-u_v1(1,:);
+     end
     if any(u2==0)
             u_v2(1,:)=-u_v2(1,:);
     end
+    %%%%%%%%%%%%%%%%%%%%%
     bangs=bangs(bangs~=0);
-     if n>0
+    if n>0  %% czas do 1 prze³¹czenia
             tmp_T=bangs(1);
             time(1,:)=linspace(tmp_t0,tmp_T,MSHDNS);
     end
+    % od 1 prze³¹czenia do ostatniego prze³¹czenia
     if n>=2
         for i=2:n
              tmp_t0=bangs(i-1); 
@@ -45,13 +47,18 @@ function [ time, u_v1,u_v2 ] = time_vectors( t, u1, u2, u0 ,MSHDNS)
 
         end
     end
+    %% od ostatniego prze³¹czenia do koñca/lub od poczatku do konca przy braku przelaczen
     if n>0
         tmp_t0=bangs(n);
         if any(tmp_t0==u1)
             u_v1(n+1,:)=-u_v1(n,:);
+        else
+            u_v1(n+1,:)=u_v1(n,:);
         end
          if any(tmp_t0==u2)
             u_v2(n+1,:)=-u_v2(n,:);
+         else
+             u_v2(n+1,:)=u_v2(n,:);
         end
     else
         tmp_t0=t(1);
